09a19b3fffcaba8f59f7b81c2fb1470791f181a9
UsEr <UsEr@DESKTOP-81TETRJ>
2020-11-11 20:28:25 +0200

    Add project files.

---
+++ homework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="homework.Global" Language="C#" %>
---
+++ homework/Page2.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace homework
+{
+    public partial class Page2 : System.Web.UI.Page
+    {
+        string a;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            a = Request.QueryString["name"];
+            TextBox1.Text = a;
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (CheckBox1.Checked == true)
+            {
+                CheckBox1.Checked = false;
+            }
+            else
+            {
+
+                CheckBox1.Checked = true;
+            }
+        }
+    }
+}
